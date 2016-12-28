2c0858b7fe142fd6b20df00714349790401388b6
林巧灵 <596075258@qq.com>
2016-12-28 14:38:04 +0800

    vue

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 /**
  * Created by linqiaoling on 16/12/22.
  */
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /**
  * Created by linqiaoling on 16/12/22.
  */
-document.write("hello world")
 require("vue")
 require("./app.less")
 require("./app.js")
